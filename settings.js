0b31cd1994acb55ed7a113a3de58de8e3e69080d
Taebearxd <149920040+Taebearxd@users.noreply.github.com>
2024-09-19 13:59:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -39,24 +39,24 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: S4 SALMAN YT" //ur yt chanel name
-global.socialm = "GitHub: salmanytofficial" //ur github or insta name
-global.location = "Pakistan, Lahore, Sabzazar" //ur location
+global.ytname = "YT: BLACKPINK" //ur yt chanel name
+global.socialm = "GitHub: Taebearxd" //ur github or insta name
+global.location = "India, West Bengal, Kolkata" //ur location
 
 //new
 global.botname = 'XLICON-V4-MD' //ur bot name
-global.ownernumber = ['923184070915'] //ur owner number, dont add more than one
-global.ownername = '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙' //ur owner name
-global.websitex = "https://youtube.com/@s4salmanyt"
-global.wagc = "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
+global.ownernumber = ['917439317360'] //ur owner number, dont add more than one
+global.ownername = '𖥻 𝚩𝚵̸̷𝚫𝐑 ꪜ' //ur owner name
+global.websitex = "https://www.instagram.com/_.bearxd?igsh=MTBiZHJ0eXNsNGJ6OA=="
+global.wagc = "https://chat.whatsapp.com/EZjCgLTJfRd8LHOyVC163u"
 global.themeemoji = '⛩'
 global.wm = "Xlicon Bot Inc."
 global.botscript = 'https://github.com/salmanytofficial/XLICON-V4-MD' //script link
-global.packname = "𝘼𝙃𝙃𝙈𝙄 𝙆𝙐𝙉"
-global.author = "MΛDΣ BY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
-global.creator = "923184070915@s.whatsapp.net"
+global.packname = "MΛDΣ BY 𖥻 𝚩𝚵̸̷𝚫𝐑 ꪜ"
+global.author = "Don't copy ‼️"
+global.creator = "917439317360@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923184070915"] // Premium User
+global.premium = ["917439317360"] // Premium User
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
@@ -95,7 +95,7 @@ global.uang = {
 }
 
 global.mess = {
-	error: 'Error!',
+	error: 'Error Baka!',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
 	done: 'Done'
 }
